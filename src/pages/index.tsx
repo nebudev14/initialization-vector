46136fd0848213603fcf0c0131cbe08666d06c1d
warren yun <wyun2006@gmail.com>
2023-07-01 05:03:36 -0400

    challenge status

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import { signIn, signOut, useSession } from "next-auth/react";
-import Head from "next/head";
-import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
