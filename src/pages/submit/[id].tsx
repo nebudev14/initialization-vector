ce80bcfd5625687158393ce2c46f5f94491f9df0
warren yun <wyun2006@gmail.com>
2023-07-06 00:39:47 -0400

    verified tag + userchallenge creation

--- src/pages/submit/[id].tsx
+++ src/pages/submit/[id].tsx
@@ -1,8 +1,8 @@
 export default function SubmitChallenge() {
 
-  
-
   return (
-
+    <div>
+      
+    </div>
   );
 }
--- src/server/api/routers/teacher-router.ts
+++ src/server/api/routers/teacher-router.ts
@@ -6,14 +6,26 @@ export const teacherRouter = createTRPCRouter({
   createChallenge: teacherProcedure
     .input(z.object({ name: z.string(), desc: z.string(), flag: z.string(), url: z.string() }))
     .mutation(async ({ ctx, input }) => {
-      return await ctx.prisma.challenge.create({
+      const challenge = await ctx.prisma.challenge.create({
         data: {
           name: input.name,
           desc: input.desc,
           flag: AES.encrypt(input.flag, process.env.AES_KEY as string).toString(),
           url: input.url,
+          users: {
+            createMany: {
+              data: (await ctx.prisma.user.findMany()).map((user) => {
+                return {
+                  status: "UNCOMPLETE",
+                  userId: user.id as string
+                }
+              })
+            }
+          }
         },
       });
+
+      return challenge;
     }),
 
   getStudents: teacherProcedure.query(async ({ ctx }) => {
