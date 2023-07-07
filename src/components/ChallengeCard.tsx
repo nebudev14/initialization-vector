22635df39295e5d9e25fce52198769d2e616c01b
warren yun <wyun2006@gmail.com>
2023-07-07 04:52:37 -0400

    challenge card component

--- src/components/ChallengeCard.tsx
+++ src/components/ChallengeCard.tsx
@@ -1,5 +1,41 @@
-export const ChallengeCard: React.FC = () => {
+import { ChallengeStatus } from "@prisma/client";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+import { ImCross } from "react-icons/im";
+
+export const ChallengeCard: React.FC<{
+  name: string;
+  desc: string;
+  status: ChallengeStatus;
+}> = ({ name, desc, status }) => {
   return (
-    <div></div>
+    <div className="my-1 w-full px-1 md:w-1/2 lg:my-4 lg:w-1/3 lg:px-4">
+      <article className="overflow-hidden rounded-2xl border border-zinc-800 shadow-xl ">
+        <div className="border-b border-yellow-300 bg-slate-900  px-4 py-6 pb-28">
+          <h1 className="font-inte mb-2 block h-auto w-full  text-3xl font-semibold">
+            {name}
+          </h1>
+          <p>{desc}</p>
+        </div>
+
+        <header className="flex items-center justify-between bg-zinc-800 p-2 leading-tight md:p-4">
+          <div className="no-underline hover:underline">
+            {status === "COMPLETED" ? (
+              <div className="flex items-center">
+                <AiOutlineCheckCircle
+                  size={20}
+                  className="mr-3 inline-block text-green-400"
+                />
+                <h1 className="text-lg">Completed</h1>
+              </div>
+            ) : (
+              <div className="flex items-center">
+                <ImCross size={20} className="mr-3 inline-block text-red-500" />
+                <h1 className="text-lg">Incomplete</h1>
+              </div>
+            )}
+          </div>
+        </header>
+      </article>
+    </div>
   );
-}
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,7 @@ import { createInnerTRPCContext } from "~/server/api/trpc";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { AiOutlineCheckCircle } from "react-icons/ai";
-import { ImCross } from "react-icons/im";
+import { ChallengeCard } from "~/components/ChallengeCard";
 
 export default function Home(
   props: InferGetServerSidePropsType<typeof getServerSideProps>
@@ -33,41 +32,12 @@ export default function Home(
                   <div className=" px-8 py-6 md:px-12 ">
                     <div className="-mx-1 flex flex-wrap lg:-mx-4 ">
                       {challenges?.map((challenge, i) => (
-                        <div
+                        <ChallengeCard
+                          name={challenge.challenge.name}
+                          desc={challenge.challenge.desc}
+                          status={challenge.status}
                           key={i}
-                          className="my-1 w-full px-1 md:w-1/2 lg:my-4 lg:w-1/3 lg:px-4"
-                        >
-                          <article className="overflow-hidden rounded-2xl border border-zinc-800 shadow-xl ">
-                            <div className="border-b border-yellow-300 bg-slate-900  px-4 py-6 pb-28">
-                              <h1 className="font-inte mb-2 block h-auto w-full  text-3xl font-semibold">
-                                {challenge.challenge.name}
-                              </h1>
-                              <p>{challenge.challenge.desc}</p>
-                            </div>
-
-                            <header className="flex items-center justify-between bg-zinc-800 p-2 leading-tight md:p-4">
-                              <div className="no-underline hover:underline">
-                                {challenge.status === "COMPLETED" ? (
-                                  <div className="flex items-center">
-                                    <AiOutlineCheckCircle
-                                      size={20}
-                                      className="mr-3 inline-block text-green-400"
-                                    />
-                                    <h1 className="text-lg">Completed</h1>
-                                  </div>
-                                ) : (
-                                  <div className="flex items-center">
-                                    <ImCross
-                                      size={20}
-                                      className="mr-3 inline-block text-red-500"
-                                    />
-                                    <h1 className="text-lg">Incomplete</h1>
-                                  </div>
-                                )}
-                              </div>
-                            </header>
-                          </article>
-                        </div>
+                        />
                       ))}
                     </div>
                   </div>
