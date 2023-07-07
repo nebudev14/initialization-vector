fd2442bc9e9d6b6436d50ba9902ab233c9c91605
warren yun <wyun2006@gmail.com>
2023-07-07 02:55:34 -0400

    lovely sign outs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,19 @@ import {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
 } from "next";
-import { getSession, useSession } from "next-auth/react";
+import { getSession, signOut, useSession } from "next-auth/react";
 import { UnsignedHome } from "~/components/UnsignedHome";
 import { appRouter } from "~/server/api/root";
 import { createInnerTRPCContext } from "~/server/api/trpc";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home(
   props: InferGetServerSidePropsType<typeof getServerSideProps>
 ) {
-
   const { user, challenges } = props;
+  const router = useRouter();
 
   return (
     <>
@@ -21,9 +24,9 @@ export default function Home(
         <UnsignedHome />
       ) : (
         <div>
-          {user?.verified ? (
-            <div className="min-h-screen px-8 py-6">
-              <div className="container mx-auto my-12 px-4 md:px-12 ">
+          {user?.verified || user?.userType === "TEACHER" ? (
+            <div className="flex min-h-screen">
+              <div className="w-8/9  container mx-auto my-12 px-8 py-6 md:px-12 ">
                 <div className="-mx-1 flex flex-wrap lg:-mx-4 ">
                   {challenges?.map((challenge, i) => (
                     <div
@@ -47,10 +50,44 @@ export default function Home(
                   ))}
                 </div>
               </div>
+              <div className=" sticky flex w-1/6 flex-col items-center bg-zinc-800 ">
+                <div className="mb-12 flex items-center justify-center px-4 py-12">
+                  <Image
+                    src={user.image as string}
+                    alt={user.name as string}
+                    width={45}
+                    height={45}
+                    className="rounded-full "
+                  />
+                  <h1 className="ml-4 text-xl">{user.name}</h1>
+                </div>
+                <div className="mb-auto">
+                  {user.userType === "STUDENT" ? (
+                    <h1 className="text-lg text-gray-400">Coming soon..</h1>
+                  ) : null}
+                </div>
+                {user.userType === "TEACHER" ? (
+                  <div
+                    onClick={() => router.push("/beaver-admin")}
+                    className="w-full bg-yellow-500 py-3 text-center duration-200 hover:cursor-pointer hover:bg-yellow-600"
+                  >
+                    <h1 className="text-xl text-gray-100">Admin Portal</h1>
+                  </div>
+                ) : null}
+                <div
+                  onClick={() => signOut()}
+                  className="w-full bg-red-500 py-3 text-center duration-200 hover:cursor-pointer hover:bg-red-600"
+                >
+                  <h1 className="text-xl text-gray-100">Sign Out</h1>
+                </div>
+              </div>
             </div>
           ) : (
-            <div className="min-h-screen flex items-center justify-center">
-              <h1 className="text-2xl">You are not a verified student! Please contact the instructors on Discord to be verified.</h1>
+            <div className="flex min-h-screen items-center justify-center">
+              <h1 className="text-2xl">
+                You are not a verified student! Please contact the instructors
+                on Discord to be verified.
+              </h1>
             </div>
           )}
         </div>
