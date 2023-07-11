8de016c6a19e5b59b0bf2fef0f88d8e8fc1c3094
warren yun <wyun2006@gmail.com>
2023-07-11 02:41:09 -0400

    fix auth errors hopefully

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -39,6 +39,7 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      checks: ['none']
     }),
 
   ],
